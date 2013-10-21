effaf2363913240a1f8b7204887ac8cc9a2d2dec
Roman <kournjaev@gmail.com>
2013-10-21 14:58:08 +0300

    adding recipe

--- site-cookbooks/youappi-base/recipes/prepare_worker.rb
+++ site-cookbooks/youappi-base/recipes/prepare_worker.rb
@@ -8,3 +8,16 @@
 #
 
 
+
+service "tomcat7" do
+  action :stop
+end
+
+remote_file "/usr/share/tomcat7/webapp/ROOT.war" do
+  source "https://s3.amazonaws.com/deploy.youappi.com/releases/latests"
+  action create_if_missing
+end
+
+service "tomcat7" do
+  action :start
+end
