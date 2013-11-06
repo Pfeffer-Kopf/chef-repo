e92748bb2390e9a007f1a76017e16756e074e3f6
Pfeffer-Kopf <root@ip-10-185-24-3.ec2.internal>
2013-11-06 15:22:20 +0000

    adding roles

--- site-cookbooks/youappi-base/recipes/init_mgn.rb
+++ site-cookbooks/youappi-base/recipes/init_mgn.rb
@@ -7,6 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'aws'
 
 ENV['YOUAPPI_HOME'] = '/youappi/central/asgard/'
 
@@ -23,6 +24,16 @@ text.gsub!(/SERVER_NAME/,"collectd-#{server}")
 File.open(file,'w') { |f| f.write(text)}
 
 
+
+aws = data_bag_item("aws", "main")
+ip_info = data_bag_item("aws", "mgn_ip")
+
+aws_elastic_ip "elastic_ip_mgn" do
+  aws_access_key aws['aws_access_key_id']
+  aws_secret_access_key aws['aws_secret_access_key']
+  ip ip_info['ip']
+  action :associate
+end
 
 
 service "collectd" do
