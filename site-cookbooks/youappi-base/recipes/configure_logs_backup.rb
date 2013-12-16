fe871e6163ed2e11974533dcb3338d744ed3c7c8
Roman <kournjaev@gmail.com>
2013-12-16 16:41:15 +0200

    configure logs backup as recipe

---
+++ site-cookbooks/youappi-base/recipes/configure_logs_backup.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: youappi-base
+# Recipe:: tag instance
+#
+# Copyright 2013, YouAPPI Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+env = node['env']
+role = node['role']
+
+aws = data_bag_item('aws', 'main')
+
+if env == 'prod' && role == 'API'
+
+ template '/etc/init.d/package_logs' do
+   source '/package_logs.erb'
+   owner 'root'
+   mode '0777'
+ end
+ 
+ link '/etc/rc0.d/S21unregister' do
+   to '/etc/init.d/package_logs'
+ end
+
+ directory "/root/.aws" do
+   owner 'root'
+   group 'root'
+   mode 00644
+   action :create
+ end
+
+ template '/root/.aws/config' do
+   source '/config_aws.erb'
+   owner  'root'
+   mode   '0777'
+   variables( 
+    	:aws_access_key_id => aws['aws_access_key_id'],
+      :aws_secret_access_key => aws['aws_secret_access_key']
+   )
+ end
+
+end
+
+
--- site-cookbooks/youappi-base/recipes/start_worker.rb
+++ site-cookbooks/youappi-base/recipes/start_worker.rb
@@ -72,39 +72,6 @@ template '/etc/init.d/unregister' do
 end
 
 
-
-if env == 'prod' && role = 'API'
- 
- template '/etc/init.d/package_logs' do
-   source '/package_logs.erb'
-   owner 'root'
-   mode '0777'
- end
- 
- link '/etc/rc0.d/S21unregister' do
-   to '/etc/init.d/package_logs'
- end
-
- directory "/root/.aws" do
-   owner 'root'
-   group 'root'
-   mode 00644
-   action :create
- end
-
- template '/root/.aws/config' do
-   source '/config_aws.erb'
-   owner  'root'
-   mode   '0777'
-   variables( 
-	:aws_access_key_id => aws['aws_access_key_id'],
-        :aws_secret_access_key => aws['aws_secret_access_key']
-   )
- end
-
-end
-
-
 if env == 'prod' && role == 'MGN'
   ip_info = data_bag_item('aws', 'mgn_ip')
   aws_elastic_ip 'elastic_ip_mgn' do
